63390c970838bf366505d6fdbba555e7e1c719a9
ocawthorne <ocawthorne@gmail.com>
2021-01-25 17:21:48 +0000

    Include Rack CORS.

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,16 @@ module SentencingCalculator
         app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
         app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
       }
+    
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end 
 
     # Configuration for the application, engines, and railties goes here.
     #
