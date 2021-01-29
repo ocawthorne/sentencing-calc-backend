ba1185a9080b4c44d79b8a12567b12f355248050
ocawthorne <ocawthorne@gmail.com>
2021-01-29 00:26:31 +0000

    Committing changes to database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_28_160944) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "counts", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
